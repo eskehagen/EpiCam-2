0cf8f036a8c1e4034ae0d5a4d9c84b3dc52ce2fe
eskehagen <eskehagen@gmail.com>
2025-07-16 19:05:16 +0200

    Added HideUI method.

--- app/src/main/java/com/example/epicam2/MainActivity.kt
+++ app/src/main/java/com/example/epicam2/MainActivity.kt
@@ -32,7 +32,6 @@ import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imageCapture: ImageCapture
@@ -56,6 +55,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hideSystemUI()
 
         window.decorView.systemUiVisibility = (
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
@@ -301,6 +301,17 @@ class MainActivity : AppCompatActivity() {
         cameraExecutor.shutdown()
 
         super.onDestroy()
+    }
+
+    /// Hide System UI (fullscreen)
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 
     /// Companion Object
